49319bfff4766d32ef95d776a0db7379c376fcba
arpit-agr <arpit.agrawal2502@gmail.com>
2022-10-15 11:13:57 +0530

    add addNbsp filter

--- .eleventy.js
+++ .eleventy.js
@@ -27,6 +27,15 @@ module.exports = function(eleventyConfig) {
       return code
     }
   });
+  eleventyConfig.addFilter("addNbsp", (str) => {
+    if (!str) {
+      return;
+    }
+    let title = str.replace(/((.*)\s(.*))$/g, "$2&nbsp;$3");
+    title = title.replace(/"(.*)"/g, '\\"$1\\"');
+    return title;
+  });
+
 
   //Transforms
   eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
